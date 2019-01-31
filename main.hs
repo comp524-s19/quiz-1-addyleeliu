5d817cbed563a96ff7173d954ad23838af09ed0c
Addy Liu <addyleeliu@gmail.com>
2019-01-31 18:01:50 +0000

    ok I think this works

--- main.hs
+++ main.hs
@@ -1,3 +1,3 @@
 finalGrade :: [Integer] -> [Integer] -> Integer
-finalGrade g = let top = zipWith (*) l1 l2
-               in (sum top) `div` (sum l2)
+finalGrade l1 l2 = let top = zipWith (*) l1 l2
+                   in (sum top) `div` (sum l2)
